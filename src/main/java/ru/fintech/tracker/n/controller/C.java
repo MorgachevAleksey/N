b1903be992fb723fcfb8d13306a32f03f95eaff3
Aleksei <morgachev_aleksey@mail.ru>
2026-09-22 18:57:47 +0300

    контроллер

--- src/main/java/ru/fintech/tracker/n/controller/C.java
+++ src/main/java/ru/fintech/tracker/n/controller/C.java
@@ -1,4 +1,12 @@
 package ru.fintech.tracker.n.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class C {
+    @GetMapping
+    public String home(){
+        return "h";
+    }
 }
